9b9cff3208ebd57302495ab72779f32628d85405
Chinthapanti <navitha.chinthapanti@gmail.com>
2024-06-19 02:28:21 +0530

    first commit

--- V3-EC2-With_VPC.tf
+++ V3-EC2-With_VPC.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-053b0d53c279acc90"
+    ami = "ami-0f58b397bc5c1f2e8"
     instance_type = "t2.micro"
-    key_name = "dpp"
+    key_name = "k8s"
     //security_groups = [ "demo-sg" ]
     vpc_security_group_ids = [aws_security_group.demo-sg.id]
     subnet_id = aws_subnet.dpp-public-subnet-01.id 
@@ -51,7 +51,7 @@ resource "aws_subnet" "dpp-public-subnet-01" {
   vpc_id = aws_vpc.dpp-vpc.id
   cidr_block = "10.1.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1a"
+  availability_zone = "ap-south-1a"
   tags = {
     Name = "dpp-public-subent-01"
   }
@@ -61,7 +61,7 @@ resource "aws_subnet" "dpp-public-subnet-02" {
   vpc_id = aws_vpc.dpp-vpc.id
   cidr_block = "10.1.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "us-east-1b"
+  availability_zone = "ap-south-1b"
   tags = {
     Name = "dpp-public-subent-02"
   }
